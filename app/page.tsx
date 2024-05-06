6a27a42dd99961256ee30823fdb1be66d9a4797e
Karl Kwok <23aw0111@jec.ac.jp>
2024-05-06 14:46:44 +0900

    added course recommend

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 import { Toggle } from "@/components/ui/toggle";
 import { Slider } from "@/components/ui/slider";
 import InlinePicker from "@/components/ui/inlinePicker";
+import { Button } from "@/components/ui/button";
+import { link } from "fs";
 
 export default function Home() {
   const [adults, setAdults] = useState(0);
@@ -20,6 +22,123 @@ export default function Home() {
   const [exitTime, setExitTime] = useState(12.5);
   const [isPackageApplied, setIsPackageApplied] = useState(false);
   const [discountedAmount, setDiscountedAmount] = useState(0);
+
+  // const calculateCost = () => {
+  //   const baseRates = weekday
+  //     ? {
+  //         adults: 1800,
+  //         children: 1300,
+  //         toddlers: 900,
+  //         infants: 300,
+  //       }
+  //     : {
+  //         adults: 2000,
+  //         children: 1400,
+  //         toddlers: 1000,
+  //         infants: 400,
+  //       };
+
+  //   const extensionRates = weekday
+  //     ? {
+  //         adults: 500,
+  //         children: 400,
+  //         toddlers: 300,
+  //         infants: 100,
+  //       }
+  //     : {
+  //         adults: 600,
+  //         children: 500,
+  //         toddlers: 400,
+  //         infants: 100,
+  //       };
+
+  //   const packageRates = {
+  //     weekday: {
+  //       "3": { adults: 2800, children: 1900, toddlers: 1400, infants: 500 },
+  //       "5": { adults: 3700, children: 2600, toddlers: 1900, infants: 700 },
+  //     },
+  //     weekend: {
+  //       "3": { adults: 3000, children: 2000, toddlers: 1500, infants: 600 },
+  //       "5": { adults: 3900, children: 2700, toddlers: 2000, infants: 800 },
+  //     },
+  //   };
+
+  //   // rate per 30 minutes
+  //   const karaokeRates = weekday
+  //     ? {
+  //         mori: 400,
+  //         umi: 450,
+  //         kaze: 450,
+  //       }
+  //     : {
+  //         mori: 450,
+  //         umi: 500,
+  //         kaze: 500,
+  //       };
+
+  //   let cost = 0;
+  //   let nonDiscountedCost = 0;
+  //   const dayType = weekday ? "weekday" : "weekend";
+
+  //   const baseCost =
+  //     adults * baseRates.adults +
+  //     children * baseRates.children +
+  //     toddlers * baseRates.toddlers +
+  //     infants * baseRates.infants;
+
+  //   let appliedDuration = 1.5; // Default duration
+  //   if (duration >= 5) {
+  //     appliedDuration = 5;
+  //   } else if (duration >= 3) {
+  //     appliedDuration = 3;
+  //   }
+
+  //   const rates =
+  //     appliedDuration > 1.5
+  //       ? packageRates[dayType][
+  //           appliedDuration.toString() as keyof (typeof packageRates)[typeof dayType]
+  //         ]
+  //       : baseRates;
+
+  //   // Calculate cost with potentially applied package
+  //   cost =
+  //     adults * rates.adults +
+  //     children * rates.children +
+  //     toddlers * rates.toddlers +
+  //     infants * rates.infants;
+
+  //   // Calculate non-discounted cost assuming no package
+  //   nonDiscountedCost = Math.ceil(duration / 1.5) * baseCost;
+
+  //   // Calculate additional cost if staying beyond the package duration
+  //   if (duration > appliedDuration) {
+  //     const additionalPeriods = Math.ceil((duration - appliedDuration) / 0.5);
+  //     cost +=
+  //       additionalPeriods *
+  //       (adults * extensionRates.adults +
+  //         children * extensionRates.children +
+  //         toddlers * extensionRates.toddlers +
+  //         infants * extensionRates.infants);
+  //   }
+
+  //   // Adding karaoke cost if selected
+  //   if (
+  //     isKaraoke &&
+  //     nonDiscountedCost > 0 &&
+  //     (karaoke === "mori" || karaoke === "umi" || karaoke === "kaze")
+  //   ) {
+  //     const karaokeCostPer30Min = karaokeRates[karaoke];
+  //     const karaokePeriods = Math.ceil(duration / 0.5);
+  //     const karaokeCost = karaokeCostPer30Min * karaokePeriods;
+  //     cost += karaokeCost;
+  //     nonDiscountedCost += karaokeCost; // Include karaoke cost in the non-discounted cost
+  //   }
+
+  //   const discountedAmount = nonDiscountedCost - cost;
+  //   setTotalCost(cost);
+  //   setDiscountedAmount(discountedAmount);
+  //   setIsPackageApplied(appliedDuration !== 1.5);
+  // };
 
   const calculateCost = () => {
     const baseRates = weekday
@@ -61,7 +180,6 @@ export default function Home() {
       },
     };
 
-    // rate per 30 minutes
     const karaokeRates = weekday
       ? {
           mori: 400,
@@ -91,14 +209,21 @@ export default function Home() {
       appliedDuration = 3;
     }
 
-    const rates =
-      appliedDuration > 1.5
-        ? packageRates[dayType][
-            appliedDuration.toString() as keyof (typeof packageRates)[typeof dayType]
-          ]
-        : baseRates;
+    // Determine if the evening offer applies
+    const isEveningOffer =
+      ((weekday && visitTime >= 15) || (weekend && visitTime >= 16)) &&
+      duration >= 3 &&
+      !isKaraoke;
 
-    // Calculate cost with potentially applied package
+    const rates = isEveningOffer
+      ? packageRates[dayType]["3"]
+      : appliedDuration > 1.5
+      ? packageRates[dayType][
+          appliedDuration.toString() as keyof (typeof packageRates)[typeof dayType]
+        ]
+      : baseRates;
+
+    // Calculate cost with potentially applied package or evening offer
     cost =
       adults * rates.adults +
       children * rates.children +
@@ -108,8 +233,8 @@ export default function Home() {
     // Calculate non-discounted cost assuming no package
     nonDiscountedCost = Math.ceil(duration / 1.5) * baseCost;
 
-    // Calculate additional cost if staying beyond the package duration
-    if (duration > appliedDuration) {
+    // Calculate additional cost if staying beyond the package duration and evening offer is not applied
+    if (duration > appliedDuration && !isEveningOffer) {
       const additionalPeriods = Math.ceil((duration - appliedDuration) / 0.5);
       cost +=
         additionalPeriods *
@@ -119,22 +244,10 @@ export default function Home() {
           infants * extensionRates.infants);
     }
 
-    // Adding karaoke cost if selected
-    if (
-      isKaraoke &&
-      (karaoke === "mori" || karaoke === "umi" || karaoke === "kaze")
-    ) {
-      const karaokeCostPer30Min = karaokeRates[karaoke];
-      const karaokePeriods = Math.ceil(duration / 0.5);
-      const karaokeCost = karaokeCostPer30Min * karaokePeriods;
-      cost += karaokeCost;
-      nonDiscountedCost += karaokeCost; // Include karaoke cost in the non-discounted cost
-    }
-
     const discountedAmount = nonDiscountedCost - cost;
     setTotalCost(cost);
     setDiscountedAmount(discountedAmount);
-    setIsPackageApplied(appliedDuration !== 1.5);
+    setIsPackageApplied(appliedDuration !== 1.5 || isEveningOffer);
   };
 
   useEffect(() => {
@@ -149,6 +262,20 @@ export default function Home() {
     duration,
     calculateCost,
   ]);
+
+  const handleReservationClick = () => {
+    const baseUrl =
+      "https://www.tablecheck.com/shops/shuminomori-higashishinjuku/reserve";
+    const paxAdult = adults;
+    const paxChild = children;
+    const paxBaby = toddlers + infants;
+
+    // Constructing the URL with query parameters
+    const url = `${baseUrl}?pax_adult=${paxAdult}&pax_child=${paxChild}&pax_baby=${paxBaby}`;
+
+    // Redirect to the constructed URL
+    window.location.href = url;
+  };
 
   return (
     <>
@@ -276,15 +403,50 @@ export default function Home() {
               {visitTime}時から{exitTime}時まで計{duration}時間
             </p>
             <h2 className="text-2xl font-bold ">
-              合計金額：¥{totalCost.toFixed(0)}
+              合計：¥{totalCost.toFixed(0)}
             </h2>
-            {isPackageApplied && (
-              <p className="text-accent-foreground">
-                {duration >= 5 ? "5" : "3"}
-                時間お得パック適用されました！
-                <br />
-                {discountedAmount}円お得！
-              </p>
+
+            {totalCost > 0 && (
+              <>
+                <p className="pt-5">おすすめコースは:</p>
+                {(weekday && visitTime >= 15 && duration >= 3 && !karaoke) ||
+                (weekend && visitTime >= 16 && duration >= 2.5 && !karaoke) ? (
+                  <p className="text-accent-foreground">
+                    {weekday
+                      ? "【平日15時〜限定】来場予約｜イブニングパック／最大4時間（ソフトドリンク飲み放題+おやつバイキング込み）"
+                      : "【16時～限定】来場予約｜イブニングパック／最大4時間（ソフトドリンク飲み放題+おやつバイキング込み）"}
+                  </p>
+                ) : (
+                  <p className="text-accent-foreground">
+                    {weekday ? "【平日限定】" : ""}
+                    {isKaraoke
+                      ? `${
+                          karaoke === "mori"
+                            ? "森"
+                            : karaoke === "umi"
+                            ? "海"
+                            : "風"
+                        }ルーム｜`
+                      : "来場予約｜"}
+                    {duration >= 4 ? "5時間" : duration <= 2 ? "90分" : "3時間"}
+                    {isKaraoke ? "／カラオケ個室プラン" : "パック"}（
+                    {isKaraoke ? "カラオケ時間内歌い放題＋" : ""}
+                    ソフトドリンク飲み放題+おやつバイキング込み）
+                  </p>
+                )}
+                {isPackageApplied && (
+                  <p className="text-accent-foreground">
+                    {discountedAmount}円お得！
+                  </p>
+                )}
+                <Button
+                  variant="outline"
+                  className="text-lg m-5"
+                  onClick={handleReservationClick}
+                >
+                  予約へ
+                </Button>
+              </>
             )}
           </div>
         </div>
